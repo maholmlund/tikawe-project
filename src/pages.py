20c652f3e580ee35b2c4bae6a02e863bc2a8b4a9
Miika Holmlund <miika.holmlund@gmail.com>
2025-03-20 14:15:51 +0200

    implement user creation

--- src/pages.py
+++ src/pages.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, redirect, session
 from flask import render_template
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 from db import Db
+from sqlite3 import IntegrityError
 import config
 
 app = Flask(__name__)
@@ -30,3 +31,19 @@ def logout():
     if "username" in session:
         del session["username"]
     return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    username = request.form["username"]
+    pwd1 = request.form["password1"]
+    pwd2 = request.form["password2"]
+    if pwd1 != pwd2:
+        return render_template("register.html", msg="passwords do not match")
+    try:
+        Db().create_user(username, generate_password_hash(pwd1))
+    except IntegrityError:
+        return render_template("register.html", msg="username already in use")
+    return redirect("/")
+    
