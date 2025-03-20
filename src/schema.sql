6505252c2ebce8d4ca2aa4eeba7ac7ff19d80665
Miika Holmlund <miika.holmlund@gmail.com>
2025-03-20 11:29:29 +0200

    create database schema

---
+++ src/schema.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Users (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        pwd_hash TEXT,
+);
+
+CREATE TABLE Posts (
+        id INTEGER PRIMARY KEY,
+        data TEXT,
+        language INTEGER REFERENCES Languages ON DELETE CASCADE,
+        user_id INTEGER REFERENCES Users ON DELETE CASCADE,
+);
+
+CREATE TABLE Languages (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+)
+
+CREATE TABLE Likes (
+        id INTEGER PRIMARY KEY,
+        user_id INTEGER REFERENCES Users ON DELTE CASCADE,
+        post_id INTEGER REFERENCES Posts ON DELETE CASCADE,
+);
+
+CREATE TABLE Comments (
+        id INTEGER PRIMARY KEY,
+        user_id INTEGER REFERENCES Users ON DELETE CASCADE,
+        post_id INTEGER REFERENCES Posts ON DELETE CASCADE,
+        data TEXT,
+);
