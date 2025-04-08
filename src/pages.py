6fb6a7ad2c5cac4d9a1d08591f61a5eb4c2d9164
Miika Holmlund <miika.holmlund@gmail.com>
2025-04-08 20:35:06 +0300

    refactor code to use g.user

--- src/pages.py
+++ src/pages.py
@@ -1,7 +1,7 @@
 from sqlite3 import IntegrityError
 from functools import wraps
 import markupsafe
-from flask import Flask, request, redirect, session
+from flask import Flask, request, redirect, session, g
 from flask import render_template, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
 from db import Db
@@ -28,6 +28,14 @@ def login_required(f):
     return decorated_function
 
 
+@app.before_request
+def add_user():
+    if "username" in session:
+        g.user = Db().get_user_by_username(session["username"])
+    else:
+        g.user = None
+
+
 @app.after_request
 def add_header(r):
     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
@@ -38,11 +46,10 @@ def add_header(r):
 
 @app.route("/", methods=["GET"])
 def index():
-    if "username" in session:
-        posts = Db().get_posts(
-            60, Db().get_user_by_username(session["username"]).id)
-        return render_template("index.html", posts=posts, username=session["username"], request=request)
-    posts = Db().get_posts(60)
+    if g.user:
+        posts = Db().get_posts(60, g.user.id)
+    else:
+        posts = Db().get_posts(60)
     return render_template("index.html", posts=posts, request=request)
 
 
@@ -109,8 +116,7 @@ def post():
             languages=Db().get_languages(),
             msg="please provide some content")
     data = request.form["data"]
-    user_id = Db().get_user_by_username(session["username"]).id
-    Db().create_post(data, language_id, user_id)
+    Db().create_post(data, language_id, g.user.id)
     return redirect("/")
 
 
@@ -118,7 +124,7 @@ def post():
 @login_required
 def edit(post_id):
     target_post = Db().get_post_by_id(post_id)
-    if target_post.username != session["username"]:
+    if target_post.username != g.user.username:
         return "invalid user", 403
     if request.method == "GET":
         languages = Db().get_languages()
@@ -136,7 +142,7 @@ def edit(post_id):
 @login_required
 def delete(post_id):
     post = Db().get_post_by_id(post_id)
-    if post.username != session["username"]:
+    if post.username != g.user.username:
         return "invalid user", 403
     Db().delete_post_by_id(post_id)
     return redirect("/")
@@ -145,45 +151,38 @@ def delete(post_id):
 @app.route("/search", methods=["GET"])
 def search():
     term = request.args.get("query")
-    if "username" in session:
+    if g.user:
         posts = Db().search_post_by_string(
-            term, 20, Db().get_user_by_username(session["username"]).id)
+            term, 20, g.user.id)
     else:
         posts = Db().search_post_by_string(term, 20)
     query = request.query_string.decode("utf-8")
-    username = session["username"] if "username" in session else None
     return render_template("search.html",
                            posts=posts,
                            request=request,
-                           query=query,
-                           username=username)
+                           query=query)
 
 
 @app.route("/like/<post_id>", methods=["POST"])
 @login_required
 def like(post_id):
-    user_id = Db().get_user_by_username(session["username"]).id
-    Db().toggle_like(post_id, user_id)
+    Db().toggle_like(post_id, g.user.id)
     if "next" not in request.form:
         return 400, "missing next field"
-    # I wish we could do this using javascript...
     query = "" if "query" not in request.form else "?" + \
         str(request.form["query"])
+    # I wish we could do this using javascript...
     return redirect(request.form["next"] + query + f"#post-{post_id}")
 
 
 @app.route("/comments/<post_id>", methods=["GET"])
 def comments(post_id):
     comments = Db().get_comments(post_id)
-    if "username" in session:
+    if g.user:
         post = Db().get_post_by_id(
-            post_id, Db().get_user_by_username(session["username"]).id)
-        return render_template("comments.html",
-                               post=post,
-                               comments=comments,
-                               hide_link=True,
-                               username=session["username"])
-    post = Db().get_post_by_id(post_id)
+            post_id, g.user.id)
+    else:
+        post = Db().get_post_by_id(post_id)
     return render_template("comments.html",
                            post=post,
                            comments=comments,
@@ -200,24 +199,26 @@ def new_comment(post_id):
                                post=post,
                                comments=comments,
                                hide_link=True,
-                               username=session["username"],
                                msg="please provide a valid comment")
-    user_id = Db().get_user_by_username(session["username"]).id
-    Db().create_comment(request.form["data"], user_id, post_id)
+    Db().create_comment(request.form["data"], g.user.id, post_id)
     return redirect("/comments/" + post_id)
 
 
 @app.route("/user/<username>", methods=["GET"])
-def user(username):
-    user = Db().get_user_by_username(username)
-    post_count = Db().get_user_post_count(user.id)
+def user_page(username):
+    target_user = Db().get_user_by_username(username)
+    post_count = Db().get_user_post_count(target_user.id)
     if "username" in session:
         posts = Db().get_posts_by_user_id(
-            user.id, 20, current_user_id=Db().get_user_by_username(session["username"]).id)
+            target_user.id,
+            20,
+            current_user_id=g.user.id)
         return render_template("user.html",
                                target_user=username,
-                               username=session["username"],
                                post_count=post_count,
                                posts=posts)
-    posts = Db().get_posts_by_user_id(user.id, 20)
-    return render_template("user.html", target_user=username, post_count=post_count, posts=posts)
+    posts = Db().get_posts_by_user_id(target_user.id, 20)
+    return render_template("user.html",
+                           target_user=username,
+                           post_count=post_count,
+                           posts=posts)
